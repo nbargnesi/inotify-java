40eb09889695ff0fdc9d1b2a6b16baa30bcd8575
Nick Bargnesi <nick@den-4.com>
2011-10-08 00:52:58 -0400

    Remove the fluctuator.

--- src/java/classes/com/den_4/inotify_java/FluctuatorListener.java
+++
@@ -1,9 +0,0 @@
-package com.den_4.inotify_java;
-
-public interface FluctuatorListener extends InotifyEventListener {
-
-    public abstract void fileSizeIncreased(final FileSizeIncreasedEvent e); 
-    
-    public abstract void fileSizeDecreased(final FileSizeDecreasedEvent e);
-    
-}
